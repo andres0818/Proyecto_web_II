fcd1ada8d7203a48d804e30454c46975e79eb9fe
Jcmedinah <jean.medina0399@gmail.com>
2026-03-06 18:34:57 -0500

    Feature: add footer component and style in css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,12 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
+        <p>&copy; 2025 EduLend. Todos los derechos reservados.</p>
+        <p>
+            <a href="https://www.edulend.com/terminos">Términos</a> |
+            <a href="https://www.edulend.com/privacidad">Privacidad</a> |
+            <a href="https://www.edulend.com/contacto">Contacto</a>
+        </p>
     </footer>
   );
 };
