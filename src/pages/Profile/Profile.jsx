72a66e767ef95649f6579e1a4e6a6c2bd3305831
Alejanrj <Alejanrj.07@gmail.com>
2026-04-24 20:25:38 -0500

    Feature: formulario controlado

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,63 +1,123 @@
+import { useState } from "react";
 import "./Profile.css";
 
 const Profile = () => {
+
+  const [formData, setFormData] = useState({
+    nombre: "",
+    apellido: "",
+    direccion: "",
+    email: "",
+    password_actual: "",
+    password_new: "",
+    password_confirmar: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>Informacion personal</legend>
+
           <label>
             Nombre:
-            <input type="text" name="nombre" />
+            <input
+              type="text"
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleChange}
+            />
           </label>
+
           <label>
             Apellido:
-            <input type="text" name="apellido" />
+            <input
+              type="text"
+              name="apellido"
+              value={formData.apellido}
+              onChange={handleChange}
+            />
           </label>
+
           <label>
-            Dirección
-            <input type="text" name="direccion" />
+            Dirección:
+            <input
+              type="text"
+              name="direccion"
+              value={formData.direccion}
+              onChange={handleChange}
+            />
           </label>
+
           <label>
             Correo Electronico:
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
           </label>
         </fieldset>
       </form>
-      <label>
-        <h3> ¿Cambiar contraseña? </h3>
-      </label>
-      <form>
+
+      <h3>¿Cambiar contraseña?</h3>
+
+      <form onSubmit={handleSubmit}>
         <fieldset>
+
           <label htmlFor="password_actual">
             Contraseña Actual
             <input
               type="password"
               name="password_actual"
               id="password_actual"
-              required=""
+              value={formData.password_actual}
+              onChange={handleChange}
+              required
             />
           </label>
+
           <label htmlFor="password_new">
             Nueva contraseña
             <input
               type="password"
               name="password_new"
               id="password_new"
-              required=""
+              value={formData.password_new}
+              onChange={handleChange}
+              required
             />
           </label>
+
           <label htmlFor="password_confirmar">
-            {" "}
             Confirmar nueva contraseña
             <input
               type="password"
               name="password_confirmar"
               id="password_confirmar"
-              required=""
+              value={formData.password_confirmar}
+              onChange={handleChange}
+              required
             />
           </label>
+
         </fieldset>
+
         <button type="submit">Guardar cambios</button>
       </form>
     </>
